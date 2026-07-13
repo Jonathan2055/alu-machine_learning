9fde5a0b9e5d163a7a0a7f51a679178e98e061f6
Jonathan2055 <j.mugisha3@alustudent.com>
2026-07-13 22:27:01 +0200

    Done Q0

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Module that calculates the likelihood of obtaining data given
+various hypothetical probabilities using a binomial distribution."""
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """
+    Calculates the likelihood of obtaining this data given various
+    hypothetical probabilities of developing severe side effects.
+
+    Args:
+        x: number of patients that develop severe side effects
+        n: total number of patients observed
+        P: 1D numpy.ndarray containing the various hypothetical
+           probabilities of developing severe side effects
+
+    Returns:
+        1D numpy.ndarray containing the likelihood of obtaining the
+        data, x and n, for each probability in P
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    n_fact = np.math.factorial(n)
+    x_fact = np.math.factorial(x)
+    nx_fact = np.math.factorial(n - x)
+    coefficient = n_fact / (x_fact * nx_fact)
+
+    likelihoods = coefficient * (P ** x) * ((1 - P) ** (n - x))
+
+    return likelihoods
