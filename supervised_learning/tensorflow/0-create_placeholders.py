0ee8bd9900daa0662ec7e6035baf7d194510b9a7
Jonathan2055 <j.mugisha3@alustudent.com>
2026-09-13 12:46:23 +0200

    added shape to placeholdrs

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""Creates placeholders for a neural network."""
+
 
 import tensorflow as tf
+"""Creates placeholders for a neural network."""
 
 
 def create_placeholders(nx, classes):
-    x = tf.placeholder(tf.float32)
-    y = tf.placeholder(tf.float32)
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
     return x, y
